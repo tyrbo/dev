815ccb7dd683309246b97429e48d9d863c7f50bb
Jonmichael Chambers <jon@tyrbo.net>
2017-01-05 00:22:15 -0700

    More work

--- dockerfiles/dev/entrypoint.sh
+++ dockerfiles/dev/entrypoint.sh
@@ -6,4 +6,10 @@ fi
 
 /home/dev/.rbenv/bin/rbenv global $RUBY_VERSION
 
+if [ ! -d "/home/dev/.nodenv/versions/$NODE_VERSION" ]; then
+  /home/dev/.nodenv/bin/nodenv install $NODE_VERSION
+fi
+
+/home/dev/.nodenv/bin/nodenv global $NODE_VERSION
+
 $@
